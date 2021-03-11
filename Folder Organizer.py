200a07e24fa7bf34efa8ee9e05c4d6d207d1cb3c
hosua <hosua.o@gmail.com>
2021-03-10 19:43:13 -0500

    Update Folder Organizer.py

--- Folder Organizer.py
+++ Folder Organizer.py
@@ -1,6 +1,8 @@
 import os, string, shutil
-# This program organizes every file in the directory into alphabetized folders.
-root = input("Enter the directory path you want to organize")
+# This program takes all files in a directory and moves them to the root.
+# NOTE: This does not handle directories within directories
+# Perhaps implement this another time
+root = input("Enter the directory path you want to extract")
 backup = input("Do you want to backup your original files? (Y/N)").lower()
 if backup == "y":
     make_backup = True
@@ -9,56 +11,61 @@ elif backup == "n":
 else:
     print("Invalid input")
     quit()
-dir_organized_folder = os.path.join(root, 'Organized folder')
+
 dir_backup_folder = os.path.join(root, 'Backup')
+
 os.chdir(root)
+if make_backup:     # I have no idea why this is causing an error. But it's working lol
+    if os.path.exists(dir_backup_folder):     # Make this folder if it doesnt exist
+        print("Deleting old backup folder")
+        os.remove(dir_backup_folder)
 
-if not os.path.exists(dir_organized_folder):     # Make this folder if it doesnt exist
-    os.makedirs('Organized folder')
-if make_backup:
-    if not os.path.exists(dir_backup_folder):     # Make this folder if it doesnt exist
-        os.makedirs('Backup')
+    os.chdir(root)
+    if not os.path.exists("#"):     # Make all the folders
+        os.makedirs("#")
+    for letter in string.ascii_uppercase:   # ignore case sensitivity
+        if not os.path.exists(letter):
+            os.makedirs(letter)
 
-os.chdir(dir_organized_folder)   # Move to directory
+    shutil.copytree(root, dir_backup_folder)    # Copy everything to backup folder
+    for directory in os.listdir(root):   # Delete all except backup folder
+        if os.path.basename(directory) != "Backup":
+            if os.path.isdir(directory):
+                print("Removing " + directory)
+                shutil.rmtree(directory)
 
+    os.chdir(dir_backup_folder)
+    for directory in os.listdir(dir_backup_folder):     # for each directory in the backup
+        if os.path.isdir(os.path.join(dir_backup_folder, directory)):
+            os.chdir(os.path.join(dir_backup_folder, directory))
+            for file in os.listdir(os.path.join(dir_backup_folder, directory)):  # copying all files to root
+                print("Copying " + file + " to " + root)
+                if os.path.isfile(file):
+                    shutil.copy(file, root)
+                else:   # directory
+                    if os.path.exists(root):
+                        shutil.rmtree(root)
+                        shutil.copytree(file, root)
+        os.chdir(root)
 
-if not os.path.exists("#"):     # Make all the folders
-    os.makedirs("#")
-for letter in string.ascii_uppercase:   # ignore case sensitivity
-    if not os.path.exists(letter):
-        os.makedirs(letter)
+else:   # Not making backup
+    for directory in os.listdir(root):     # for each directory in the root
+        if os.path.isdir(os.path.join(root, directory)):
+            os.chdir(os.path.join(root, directory))
+            for file in os.listdir(os.path.join(root, directory)):  # moving all files to root
+                print("Moving " + file + " to " + root)
+                if os.path.isfile(file):
+                    shutil.move(file, root)
+                else:  # directory
+                    shutil.move(file, root)
+        os.chdir(root)
 
-os.chdir(root)  # We need to go into root
-for file in os.listdir(root):
-    if os.path.isfile(os.path.join(root, file)):  # Check if path is a file
-        print(file + " is a file")
-        filename = file.rsplit()    # Gets only name of file
-        firstLetter = filename[0].upper()[0]
-        if firstLetter in string.ascii_letters:
-            new_path = os.path.join(dir_organized_folder, firstLetter)
-            print(new_path)
-            if not make_backup:
-                shutil.move(file, new_path)     # Copying files to each directory
-                print("Moving " + file + " to " + new_path)
-            else:
-                shutil.copy(file, new_path)     # Copying files to each directory
-                print("Copying " + file + " to " + new_path)
-                shutil.move(file, dir_backup_folder)    # Move files into a backup folder
-                print("Moving " + file + " to " + dir_backup_folder)   # use this if you want to make a backup folder
-        else:
-            new_path = os.path.join(dir_organized_folder, "#")
-            if make_backup:
-                shutil.copy(file, dir_backup_folder)
-                print("Moving " + file + " to " + dir_backup_folder)
-            shutil.move(file, new_path)
-            print("Moving " + file + " to " + new_path)
+for directory in os.listdir(root):
+    if os.path.isdir(directory):    # check if a directory first
+        if len(os.listdir(directory)) == 0:     # remove directory if its empty
+            print("Removing " + directory)
+            shutil.rmtree(directory)
 
-
-os.chdir(dir_organized_folder)  # Go into the new organized folder
-for directory in os.listdir(dir_organized_folder):  # Delete any directories that are empty
-    if len(os.listdir(directory)) == 0:
-        shutil.rmtree(directory)
-        print("Deleting '" + os.path.join(root, directory) + "' because it was empty")
 
 
 
